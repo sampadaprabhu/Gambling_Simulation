21b6309c62ef9a7dd64a375eaa21b45ec62580ab
sampadaprabhu <sampadaprabhu98@gmail.com>
2020-01-28 10:15:51 +0530

    ADDED: Gambler Win or Loose Condition

--- Gambling_Simulator.sh
+++ Gambling_Simulator.sh
@@ -4,3 +4,12 @@ echo "Welcome To Gambler Simulator"
 #Declairing Constant Values
 STAKE_OF_EVERY_DAY=100
 BET_OF_EVERY_GAME=1
+Cash=$STAKE_OF_EVERY_DAY
+if ((RANDOM%2 == 0))
+		then
+			echo "Player is Win!!"
+				Cash=$(( $Cash + $BET_OF_EVERY_GAME ))
+		else
+			echo "Player is loss!!"
+				Cash=$(( $Cash - $BET_OF_EVERY_GAME ))
+		fi
