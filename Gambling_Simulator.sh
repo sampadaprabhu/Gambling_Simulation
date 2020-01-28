d510f0652f0e12ad2e5ac7ca53f8f99b3e7a2d5f
sampadaprabhu <sampadaprabhu98@gmail.com>
2020-01-28 11:12:56 +0530

    ADDED: Calculating Total Win And Loose Cash Using WinOrLoose Function

--- Gambling_Simulator.sh
+++ Gambling_Simulator.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
 echo "Welcome To Gambler Simulator"
-
 percentage()
 {
 	percentage=$(( $STAKE_OF_EVERY_DAY * 50 / 100 ))
 	Minimum_percentage=$(( $STAKE_OF_EVERY_DAY - $percentage ))
 	Maximum_percentage=$(( $STAKE_OF_EVERY_DAY + $percentage ))
 }
-
+WinOrLoose()
+{
+	if (( $Cash > 100 ))
+	then
+		Win=$(( $Win + 50 ))
+	else
+		Loss=$(( $Loss + 50 ))
+	fi	
+}
 #Declairing Constant Values
 STAKE_OF_EVERY_DAY=100
 BET_OF_EVERY_GAME=1
-Cash=$STAKE_OF_EVERY_DAY
-	percentage
-while (( $Cash > Minimum_percentage && $Cash < $Maximum_percentage ))
+Win=0
+Loss=0
+for (( i=1; i<=20; i++ ))
 do
+	Cash=$STAKE_OF_EVERY_DAY
+	percentage
+	while (( $Cash > Minimum_percentage && $Cash < $Maximum_percentage ))
+	do
 		if ((RANDOM%2 == 0))
 		then
 			Cash=$(( $Cash + $BET_OF_EVERY_GAME ))
 		else
 			Cash=$(( $Cash - $BET_OF_EVERY_GAME ))
 		fi
+	done
+	WinOrLoose
 done
-echo "Total Cash is : $Cash"
+echo "Total Winning Cash: $Win"
+echo "Total Loosing Cash: $Loss"
