fabd8cb5ec2640f2554586af420fa68a2cecd7e7
sampadaprabhu <sampadaprabhu98@gmail.com>
2020-01-28 10:33:35 +0530

    ADDED: Percentage Function And Displaying Resigning Cash

--- Gambling_Simulator.sh
+++ Gambling_Simulator.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 echo "Welcome To Gambler Simulator"
 
+percentage()
+{
+	percentage=$(( $STAKE_OF_EVERY_DAY * 50 / 100 ))
+	Minimum_percentage=$(( $STAKE_OF_EVERY_DAY - $percentage ))
+	Maximum_percentage=$(( $STAKE_OF_EVERY_DAY + $percentage ))
+}
+
 #Declairing Constant Values
 STAKE_OF_EVERY_DAY=100
 BET_OF_EVERY_GAME=1
 Cash=$STAKE_OF_EVERY_DAY
-if ((RANDOM%2 == 0))
+	percentage
+while (( $Cash > Minimum_percentage && $Cash < $Maximum_percentage ))
+do
+		if ((RANDOM%2 == 0))
 		then
-			echo "Player is Win!!"
-				Cash=$(( $Cash + $BET_OF_EVERY_GAME ))
+			Cash=$(( $Cash + $BET_OF_EVERY_GAME ))
 		else
-			echo "Player is loss!!"
-				Cash=$(( $Cash - $BET_OF_EVERY_GAME ))
+			Cash=$(( $Cash - $BET_OF_EVERY_GAME ))
 		fi
+done
+echo "Total Cash is : $Cash"
